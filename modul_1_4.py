2ad2320524d6c00720aad3979bb6c1b38cd09c14
Tatevik <tatevik9330@gmail.com>
2024-12-03 19:03:43 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+hours_spent = 1.5
+cours = 'Python'
+time_for_one_lesson = hours_spent / total_tasks
+print('Курс:', cours, ', ' 'всего задач:', total_tasks, ', ''затрачено часов:', hours_spent, ', ''среднее время выполнения:', time_for_one_lesson)
